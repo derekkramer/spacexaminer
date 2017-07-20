ab91b657307c0472d528d9df89115ae3633f9434
Derek Kramer <derek.kramer88@gmail.com>
2017-07-20 11:54:00 -0600

    ✅ Added capstone presentation information

--- app/public/scripts/index.js
+++ app/public/scripts/index.js
@@ -1,3 +1,6 @@
+let techView = 0,
+    contactView = 0;
+
 $('.viewer').click(() => {
     $('.content').removeClass('show');
 });
@@ -31,3 +34,40 @@ $(window).on('load', () => {
 $('.manifest').click(() => {
     window.location = '/manifest';
 });
+
+$(window).keydown((event) => {
+    if(event.which === 84){
+        techView++;
+
+        if(techView === 1){
+            $('#node').css('visibility', 'visible');
+        }else if(techView === 2){
+            $('#express').css('visibility', 'visible');
+        }else if(techView === 3){
+            $('#sass').css('visibility', 'visible');
+        }else if(techView === 4){
+            $('#three').css('visibility', 'visible');
+        }else if(techView === 5){
+            $('#webgl').css('visibility', 'visible');
+        }else if(techView === 6){
+            $('.tech-top > img').css('visibility', 'hidden');
+            $('.tech-bottom > img').css('visibility', 'hidden');
+            techView = 0;
+        }
+    }else if(event.which === 67){
+        contactView++;
+
+        if(contactView === 1){
+            $('#github').css('visibility', 'visible');
+        }else if(contactView === 2){
+            $('#linkedin').css('visibility', 'visible');
+        }else if(contactView === 3){
+            $('#email').css('visibility', 'visible');
+        }else if(contactView === 4){
+            $('#phone').css('visibility', 'visible');
+        }else if(contactView === 5){
+            $('.contact > div').css('visibility', 'hidden');
+            contactView = 0;
+        }
+    }
+})
